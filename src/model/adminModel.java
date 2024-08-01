c5f3300ceaa1b4623bf60af32986bf037e999935
ASUS <AUNGMYO.23@ichat.sp.edu.sg>
2024-08-01 16:07:06 +0800

    Hello World Test

--- src/model/adminModel.java
+++ src/model/adminModel.java
@@ -2,14 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package javaasssignment;
+package model;
 
 /**
  *
  * @author ASUS
  */
-public class Hello {
-    public static void main(String[] args) {
-            System.out.println("kill mes");
-    }
+public class adminModel {
+    
 }
